3c8d02eb36ac674b98e7ea0672e3c19d3bc7f2ea
Masonk7 <150885585+Masonk7@users.noreply.github.com>
2025-11-13 11:11:25 -0600

    added imports for app

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import json
+import webbrowser
+from datetime import datetime, date
+import requests
+import pygal
+from lxml import etree
+import csv
+from flask import Flask, render_template, request
